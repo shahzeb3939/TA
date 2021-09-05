d4217a06bf23d1cae59edbd608f14df31b04eaed
shahzeb <shahzeb.khan@emumba.com>
2021-09-05 17:03:45 +0500

    first commit

---
+++ src/test/java/com/TA/acceptancetests/AcceptanceTestSuite.java
@@ -0,0 +1,9 @@
+package com.TA.acceptancetests;
+
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(features="src/test/resources/features/", glue="com.TA")
+public class AcceptanceTestSuite {}
